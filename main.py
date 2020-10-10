b32a1a32e55e3ddb34d7f3dbb90dd2d9f4cda286
Boberkraft <andrzej.bisewski@gmail.com>
2020-10-10 21:23:11 +0200

    losowy zart

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
+import random
+
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-skrypt = {"skrypt": """
-        let div = document.getElementById('moj-hajs');
-        div.innerHTML = parseInt(div.innerHTML) + 1;
-    """}
+js_zwiekszajacy_hajs = { "skrypt": """
+    let div = document.getElementById('moj-hajs');
+    div.innerHTML = parseInt(div.innerHTML) + 1;
+"""}
 
     
 @app.get("/odswierz")
-def read_item():
-    return skrypt
+def odswierz():
+    return js_zwiekszajacy_hajs
 
 @app.get("/zwieksz_hajs")
-def read_item():
-    return skrypt
+def zwieksz_hajs():
+    return js_zwiekszajacy_hajs
+
+@app.get("/daj_zart")
+def daj_zart():
+    zarty = [
+        'Wiesz jak ma na imie moja mama? Andrzej!',
+        'Wiesz czemu nie ma mnie w domu? Bo sram!',
+        'Wiesz za co kain zabił abla? Za ciągniecie kabla!',
+        'Hehehehe'
+    ]
+    return { "zart": random.sample(zarty, 1) }
